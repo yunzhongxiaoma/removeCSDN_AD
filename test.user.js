0b10991cdd351db4bbcb21bc232909b6d55d78fd
MaYan <i_need_you0.0@qq.com>
2018-10-01 12:39:04 +0800

    增加了隐藏底部广告代码

--- test.user.js
+++ test.user.js
@@ -1,2 +1,18 @@
-var child=document.getElementById("cpro_u2734133");
-child.parentNode.removeChild(child);
+//侧上方广告栏
+var child0=document.getElementById("cpro_u2734133");
+if(child0 != null){
+	child0.parentNode.removeChild(child0);
+}
+//侧下方广告栏,跪了OTZ
+/*
+var child1=document.getElementById("asideFooter").firstChild;
+if(child1 != null){
+    child1.parentNode.removeChild(child1);
+}
+*/
+
+//底部广告栏
+var child2=document.getElementById("haha");
+if(child2 != null){
+	child2.parentNode.parentNode.parentNode.style.display = "none";
+}
